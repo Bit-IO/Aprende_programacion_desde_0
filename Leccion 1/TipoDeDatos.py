1f8102bfc2310a49d792ba5e216b39d769bc7f1b
Bit-IO <145951094+Bit-IO-learning@users.noreply.github.com>
2024-09-26 15:36:35 -0600

    Delete Leccion 1 directory

--- Leccion 1/TipoDeDatos.py
+++
@@ -1,4 +0,0 @@
-print(type(32)) # esto devuelve <class 'int'>
-print(type(73.2)) # esto devuelve <class 'float'>
-print(type("Hola")) # esto devuelve <class 'str'>
-print(type(True)) # esto devuelve <class 'bool'>
